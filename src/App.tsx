214fbd37eda7f579f39522291fce012b1b3f3b44
Quadatz <quadath@gmail.com>
2023-06-12 11:56:27 +0300

    simplest app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useAppSelector, useAppDispatch } from './hooks';
+import { decrement, increment } from './features/counterSlice';
 
 function App() {
+
+  const count = useAppSelector((state) => state.counter.value)
+  const dispatch = useAppDispatch()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{count}</p>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
     </div>
   );
 }
